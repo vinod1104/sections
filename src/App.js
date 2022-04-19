33b637488a5f05ec806548bfa27d355a3c83c2b9
vinod1104 <81498161+vinod1104@users.noreply.github.com>
2022-04-19 11:48:09 +0530

    Add files via upload
    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Figma from './components/Figma';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Figma/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Figma.js
@@ -0,0 +1,116 @@
+import React from "react";
+const redirection = ()=>{
+   alert("Content was Clicked")
+}
+function Figma(){
+   return(
+      <div>
+  
+      <div className="section-container">
+         <div className="columns content">
+            <div  className="content-container">
+            <h5 onClick={redirection}>What we provide</h5>
+               <p>
+                  It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+               </p>
+            </div>
+         </div>
+         <div className="columns image" 
+               style={{backgroundImage: `url("https://images.unsplash.com/photo-1547082299-de196ea013d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGNvbXB1dGVyfGVufDB8fDB8fA%3D%3D&w=1000&q=80")`}}>
+            &nbsp;
+         </div>
+      </div>
+      
+      <div className="section-container">
+         <div className="columns image" 
+            style={{backgroundImage: `url("https://images.unsplash.com/photo-1551739440-5dd934d3a94a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGNvbXB1dGVyfGVufDB8fDB8fA%3D%3D&w=1000&q=80")`}}>
+            &nbsp;
+         </div>
+         <div className="columns content">
+            <div  className="content-container">
+               <h3 style={{color:"burlywood"}}>DEF!</h3>
+               <h5 onClick={redirection} >Scale of world</h5>
+               <p>
+                  It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+               </p>
+               
+            </div>
+         </div>
+      </div>
+      <div className="section-container">
+         <div className="columns content">
+            <div className="content-container">
+               <h3 style={{color:"red"}}>DAPPS</h3>
+               <h5 onClick={redirection} >Technology made easier</h5>
+               <p>
+                  It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+               </p>
+            </div>
+         </div>
+         <div className="columns image" style={{backgroundImage:`url("https://images.unsplash.com/photo-1547082299-de196ea013d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGNvbXB1dGVyfGVufDB8fDB8fA%3D%3D&w=1000&q=80")`}}>
+            &nbsp;
+         </div>
+      </div>
+      <div className="section-container">
+         <div className="columns image" style={{backgroundImage:`url("https://images.unsplash.com/photo-1551739440-5dd934d3a94a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGNvbXB1dGVyfGVufDB8fDB8fA%3D%3D&w=1000&q=80")`}}>
+            &nbsp;
+         </div>
+         <div className="columns content">
+            <div className="content-container">
+               <h3 style={{color:"yellowgreen"}}>AUDIT</h3>
+               <h5 onClick={redirection}>Stay Secure Stay Safe</h5>
+               <p>
+                  It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+               </p>
+               
+            </div>
+         </div>
+      </div>
+      <div className="section-container">
+         <div className="columns content">
+            <div className="content-container">
+               <h3 style={{color:"pink"}}>EXPERT</h3>
+               <h5 onClick={redirection}>Expert Review</h5>
+               <p>
+                  It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+               </p>
+            </div>
+         </div>
+         <div className="columns image" style={{backgroundImage:`url("https://images.unsplash.com/photo-1547082299-de196ea013d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGNvbXB1dGVyfGVufDB8fDB8fA%3D%3D&w=1000&q=80")`}}>
+            &nbsp;
+         </div>
+      </div>
+      <div className="section-container">
+         <div className="columns image" style={{backgroundImage:`url("https://images.unsplash.com/photo-1551739440-5dd934d3a94a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGNvbXB1dGVyfGVufDB8fDB8fA%3D%3D&w=1000&q=80")`}}>
+            &nbsp;
+         </div>
+         <div className="columns content">
+            <div className="content-container">
+               <h3 style={{color:"lightgoldenrodyellow"}}>SMART CONTRACT</h3>
+               <h5 onClick={redirection}>Experience The Revolution</h5>
+               <p>
+                  It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+               </p>
+               
+            </div>
+         </div>
+      </div>
+      <div className="section-container">
+         <div className="columns content">
+            <div   className="content-container">
+               <h3 style={{color:"gray"}}>LAUNCHPAD</h3>
+               <h5 onClick={redirection}>Stabilize and Promote</h5>
+               <p>
+                  It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+               </p>
+            </div>
+         </div>
+         <div className="columns image" style={{backgroundImage:`url("https://images.unsplash.com/photo-1547082299-de196ea013d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGNvbXB1dGVyfGVufDB8fDB8fA%3D%3D&w=1000&q=80")`}}>
+            &nbsp;
+         </div>
+      </div>
+   </div>
+   )
+}
+
+export default Figma;
